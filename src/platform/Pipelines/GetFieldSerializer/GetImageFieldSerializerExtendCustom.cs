983b9e1d8a07dd243f1de54112afe36c073c0f1c
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-08-28 11:50:35 +1000

    some changes

---
+++ src/platform/Pipelines/GetFieldSerializer/GetImageFieldSerializerExtendCustom.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Connector.ContentHub.DAM.FieldSerializers;
+using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Serialization;
+using Sitecore.LayoutService.Serialization.FieldSerializers;
+using Sitecore.LayoutService.Serialization.Pipelines.GetFieldSerializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XmCloudSXAStarter.Pipelines.GetFieldSerializer
+{
+    public class GetImageFieldSerializerExtendCustom : BaseGetFieldSerializer
+    {
+        public GetImageFieldSerializerExtendCustom(IFieldRenderer fieldRenderer)
+          : base(fieldRenderer)
+        {
+        }
+
+        protected override void SetResult(GetFieldSerializerPipelineArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            args.Result = (IFieldSerializer)new ImageFieldSerializerExtendCustom(this.FieldRenderer);
+        }
+    }
+}
