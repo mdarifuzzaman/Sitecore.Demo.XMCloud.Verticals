b7e456da5cef8eb9ae11c5219be996de3673ea08
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-09-05 13:57:13 +1000

    push all changes

---
+++ src/platform/Events/PublishEnd/SampleEvent.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Publishing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace XmCloudSXAStarter.Events.PublishEnd
+{
+    public class SampleEvent
+    {
+        public void OnPublishEnd(object sender, EventArgs args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            Sitecore.Diagnostics.Log.Audit("SampleEvent -> OnPublishEnd: ", this);
+            var sitecoreArgs = args as Sitecore.Events.SitecoreEventArgs;
+            if (sitecoreArgs == null)
+                return;
+
+            var publisher = sitecoreArgs.Parameters[0] as Publisher;
+            if(publisher == null) return;
+            var rootItem = publisher.Options.RootItem;
+            
+            Sitecore.Diagnostics.Log.Warn("Root item found: " + rootItem, this);
+            stopWatch.Stop();
+            Sitecore.Diagnostics.Log.Warn("Finishing up. Total elapsed time: " + stopWatch.Elapsed, this);
+            Sitecore.Diagnostics.Log.Warn("Custom settings value: " + Sitecore.Configuration.Settings.GetSetting("CustomSettingViaEnv", "No values"), this);
+        }
+    }
+}
