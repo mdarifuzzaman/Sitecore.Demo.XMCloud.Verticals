849c0cdbc50b802868acda4f85895e6cb1052d67
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-09-17 22:08:09 +1000

    some updates

---
+++ src/sxastarter/src/components/NestedComponentContainer.tsx
@@ -0,0 +1,23 @@
+
+import { ComponentParams, ComponentRendering, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import React from 'react'
+
+interface ComponentProps {
+    rendering: ComponentRendering & { params: ComponentParams };
+    params: ComponentParams;
+  }
+
+export default function NestedComponentContainer(props: ComponentProps) {
+  console.log("NestedComponentContainer", props);
+  const phKey = `container-${props.params.DynamicPlaceholderId}`;
+  const id = props.params.RenderingIdentifier;
+  return (
+    <div className={`component container-default`} id={id ? id : undefined}>
+      <div className="component-content">
+        <div className="row">
+          <Placeholder name={phKey} rendering={props.rendering} />
+        </div>
+      </div>
+    </div>
+  )
+}
