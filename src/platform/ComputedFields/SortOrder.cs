3d282292ffa4ebe21dc14d1b621dea95aba5fb4f
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-11-01 14:41:02 +1100

    updated sortorder

---
+++ src/platform/ComputedFields/SortOrder.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.ContentSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace XmCloudSXAStarter.ComputedFields
+{
+    public class SortOrder : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = (Item)(indexable as SitecoreIndexableItem);
+            return (item == null) ? (object)null : (object)item[Sitecore.FieldIDs.Sortorder];
+        }
+    }
+}
